258757c491bd89840ee02b00d8b740d459e6f435
Vasileva Anna <vasileva.ana@outlook.com>
2021-10-19 13:01:29 +0300

    buttons open pop-ups

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const likeButton = document.querySelector(".card__like-button");
+const profile = document.querySelector(".profile");
+const profileEditButton = profile.querySelector(".profile__edit-button");
 
-likeButton.addEventListener("click", function () {
-  console.log("Like button was clicked");
+const popUp = document.querySelector(".pop-up");
+const popUpCloseButton = popUp.querySelector(".pop-up__close-button");
+
+profileEditButton.addEventListener("click", function () {
+  popUp.classList.add("pop-up_opened");
+});
+
+popUpCloseButton.addEventListener("click", function () {
+  popUp.classList.remove("pop-up_opened");
+});
+
+const profileAddButton = profile.querySelector(".profile__add-button");
+
+profileAddButton.addEventListener("click", function () {
+  popUp.classList.add("pop-up_opened");
 });
